15d4c039a1bb376837fcd527900e96e241f44c22
Peter Irlam <irlampeter@gmail.com>
2020-09-05 14:01:08 +0100

    Add contacts array

---
+++ contacts.js
@@ -0,0 +1,38 @@
+let myContacts = [
+	{
+		firstName: 'Milly',
+		lastName: 'Brown',
+		number: '07232667531',
+		relationship: 'Married',
+		pets: 'Dogs',
+		likes: [ 'Ice cream', 'Reading trashy novels', 'Fine wine' ],
+		interests: [ 'Crochet', 'Keep fit', 'Breeding Weimaraners' ]
+	},
+	{
+		firstName: 'Alex',
+		lastName: 'Sumner',
+		number: '07948625340',
+		relationship: 'Married',
+		pets: 'Guinea pigs',
+		likes: [ 'Religion', 'Craft Beers', 'Amateur magic' ],
+		interests: [ 'JW', 'Interior design', 'Gaming' ]
+	},
+	{
+		firstName: 'Lisa',
+		lastName: 'Polfor',
+		number: '07947652334',
+		relationship: 'Single',
+		pets: 'None',
+		likes: [ '80s music', 'Weddings' ],
+		interests: [ 'Cheerleading', 'Outdoors' ]
+	},
+	{
+		firstName: 'Phil',
+		lastName: 'Withens',
+		number: '07986533891',
+		relationship: 'Divorced',
+		pets: 'None',
+		likes: [ 'Coding', 'Eating out' ],
+		interests: [ 'Hunting', 'Financial markets', 'Wearable tech' ]
+	}
+];
